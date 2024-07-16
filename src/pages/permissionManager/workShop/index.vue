8d9d1c76953d0576c57d9f9682759558bbc90d5f
本心 <377079544@qq.com>
2024-07-16 11:20:05 +0800

    feat(workshop): 添加公司编辑功能
    - 引入新的公司配置编辑模态框。
    - 更新表格操作列，使用新的 `showModal` 方法来显示相应的工作坊配置编辑模态框。
    - 增加公司配置的表单字段，包括公司名称、编码、别名和状态。
    - 实现 `updateWorkShop` 方法来处理工作坊配置的更新操作。
    - 相应地更新了列定义，以展示工作坊的状态使用Tag组件来呈现启用或禁用状态。
    
    此功能使用户能够方便地编辑工作坊的配置信息，提高了系统管理的灵活性和可用性。

--- src/pages/permissionManager/workShop/index.vue
+++ src/pages/permissionManager/workShop/index.vue
@@ -13,12 +13,26 @@
             </template>
             <t-table :columns="columns" row-key="id" :data="workShopList">
                 <template #operation="{ row }">
-                    <t-button theme="primary" variant="text" @click="showWorkShopModal(row)">编辑</t-button>
+                    <t-button theme="primary" variant="text" @click="showModal(row, 'workShop')">编辑</t-button>
                     <t-button theme="danger" variant="text" @click="deleteWorkShop(row)">删除</t-button>
                 </template>
             </t-table>
         </t-card>
-        <t-dialog v-model:visible="workShopModal" @confirm="updateConfig" header="配置编辑器" width="800">
+        <t-dialog v-model:visible="workShopModal" @confirm="updateWorkShop" header="配置编辑器" width="800">
+            <t-form>
+                <t-form-item label="公司名称" name="name">
+                    <t-input v-model="workShopItem.name" />
+                </t-form-item>
+                <t-form-item label="公司编码" name="code">
+                    <t-input v-model="workShopItem.code" />
+                </t-form-item>
+                <t-form-item label="公司别名" name="aliasName">
+                    <t-input v-model="workShopItem.aliasName" />
+                </t-form-item>
+                <t-form-item label="状态" name="active">
+                    <t-switch v-model="workShopItem.active" :custom-value="[1, 0]" />
+                </t-form-item>
+            </t-form>
         </t-dialog>
     </div>
 </template>
@@ -27,13 +41,28 @@
 import { ref, onMounted } from 'vue'
 import { getAPI } from '@/axios-utils';
 import { SysworkshopApi } from '@/api-services'
-import { MessagePlugin } from 'tdesign-vue-next'
+import { MessagePlugin, Tag } from 'tdesign-vue-next'
 
 const workShopModal = ref(false)
 const workShopList = ref([{ id: -1, name: '', code: '', aliasName: '', active: 1 }])
 const workShopItem = ref({ id: -1, name: '', code: '', aliasName: '', active: 1 })
 const AddWorkShopItem = ref({ name: '', code: '', aliasName: '', active: 1 })
 
+const columns = [{ title: 'ID', colKey: 'id', width: 100 }
+    , { title: '公司名称', colKey: 'name' }
+    , { title: '公司编码', colKey: 'code' }
+    , { title: '公司别名', colKey: 'aliasName' }
+    , {
+    title: '状态', colKey: 'active', cell: (h, { row }) => {
+        //  return h('t-switch', row.active == 1 ? '启用' : '禁用')
+        return h(Tag, {
+            theme: row.active == 1 ? 'success' : 'danger',
+        }, () => row.active == 1 ? '启用' : '禁用')
+
+    }
+}
+    , { title: '操作', colKey: 'operation' }
+]
 
 onMounted(() => {
     getWorkShopList();
@@ -52,6 +81,41 @@ const addWorkShop = async () => {
         workShopModal.value = false
         getWorkShopList()
     })
+}
+/* 更新公司信息 */
+const updateWorkShop = async () => {
+    getAPI(SysworkshopApi).apiSysworkshopWrokshopPut(workShopItem.value).then(res => {
+        if (res.data.statusCode == 200) {
+            MessagePlugin.success('更新成功')
+        }
+        getWorkShopList();
+        hideModal('workShop')
+    })
+}
+/* 显示弹窗 */
+const showModal = (row, res) => {
+    switch (res) {
+        case 'workShop':
+            workShopModal.value = true
+            workShopItem.value = row
+            console.warn(workShopItem.value)
+            break;
+
+        default:
+            break;
+    }
+
+}
+/* 隐藏弹窗 */
+const hideModal = (res) => {
+    switch (res) {
+        case 'workShop':
+            workShopModal.value = false
+            break;
+
+        default:
+            break;
+    }
 }
 
 </script>
