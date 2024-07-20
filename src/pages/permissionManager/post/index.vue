9070c36593bc748252bc78f771a6ed2fa3a5f9de
benxin <377079544@qq.com>
2024-07-20 21:13:17 +0800

    feat(permissionManager/post): 初始化岗位管理页面框架及获取岗位列表功能
    - 引入必要的依赖和API服务。
    - 实现页面的初始框架结构。
    - 添加获取岗位列表的异步函数，并在页面加载时调用。

--- src/pages/permissionManager/post/index.vue
+++ src/pages/permissionManager/post/index.vue
@@ -2,4 +2,15 @@
     <div></div>
 </template>
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { getAPI } from '@/axios-utils';
+import { PostApi } from '@/api-services'
+
+onMounted(() => {
+
+})
+/* 获取所有岗位 */
+const getPostList = async () => {
+    getAPI(PostApi).apiPostPostbyuserGet
+}
 </script>
