c787d7b62bc3ebd67d29c558846eb79f46c9b863
benxin <377079544@qq.com>
2024-08-04 20:58:18 +0800

    规划动态表单组件

---
+++ src/components/dy-form/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+const props = defineProps({
+    // 菜单数据
+    formData: {
+        type: Array,
+        default: () => []
+    },
+    formConfig: {
+        type: Array,
+        default: () => []
+    }
+})
+</script>
